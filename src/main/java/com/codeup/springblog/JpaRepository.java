a9ae29e62a9e5c03606fc33eba63f0ea9f0a26dd
Irfa Sheikh <irfasheikh@gmail.com>
2021-06-16 23:50:20 -0500

    today's commit

--- src/main/java/com/codeup/springblog/JpaRepository.java
+++ src/main/java/com/codeup/springblog/JpaRepository.java
@@ -1,4 +1,4 @@
 package com.codeup.springblog;
 
-public interface UserRepository {
+public class JpaRepository {
 }
--- src/main/java/com/codeup/springblog/controllers/UserController.java
+++ src/main/java/com/codeup/springblog/controllers/UserController.java
@@ -1,20 +1,54 @@
 package com.codeup.springblog.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class UserController {
+//
+//    @GetMapping("users/all")
+//    public String getAllUsers(){
+//        return "users";
+//    }
+//
+//    @GetMapping("/user")
+//    @ResponseBody
+//    public String getTheStringUser(){
+//        return "users";
+//    }
+//}
 
+
+import com.codeup.springblog.UsersRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+    private UsersRepository users;
+//    private PasswordEncoder passwordEncoder;
 
-    @GetMapping("users/all")
-    public String getAllUsers(){
-        return "users";
+//    public UserController(Users users, PasswordEncoder passwordEncoder) {
+//        this.users = users;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/sign-up";
     }
 
-    @GetMapping("/user")
-    @ResponseBody
-    public String getTheStringUser(){
-        return "users";
-    }
+//    @PostMapping("/sign-up")
+//    public String saveUser(@ModelAttribute User user){
+//        String hash = passwordEncoder.encode(user.getPassword());
+//        user.setPassword(hash);
+//        users.save(user);
+//        return "redirect:/login";
+//    }
 }
+
