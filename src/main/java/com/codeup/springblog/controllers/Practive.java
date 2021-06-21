f21c4482566e6b8c941444c54e484ac0e6d4aba3
Irfa Sheikh <irfasheikh@gmail.com>
2021-06-21 17:15:22 -0500

    practice

--- src/main/java/com/codeup/springblog/controllers/Practive.java
+++ src/main/java/com/codeup/springblog/controllers/Practive.java
@@ -1,13 +1,28 @@
 package com.codeup.springblog.controllers;
 
 public class Practive {
-    Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. However, Charlie doesn't like these sessions, since the motive usually repeats. He isn't fond of seeing the Eiffel tower 40 times. He tells them that he will only sit during the session if they show the same motive at most N times. Luckily, Alice and Bob are able to encode the motive as a number. Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
+//    Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. However, Charlie doesn't like these sessions, since the motive usually repeats. He isn't fond of seeing the Eiffel tower 40 times. He tells them that he will only sit during the session if they show the same motive at most N times. Luckily, Alice and Bob are able to encode the motive as a number. Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
+//
+//    Task
+//    Given a list lst and a number N, create a new list that contains each number of lst at most N times without reordering. For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+//
+//    Example
+//EnoughIsEnough.deleteNth(new int[] {20,37,20,21}, 1) // return [20,37,21]
+//            EnoughIsEnough.deleteNth(new int[] {1,1,3,3,7,2,2,2,2}, 3) // return [1, 1, 3, 3, 7, 2, 2, 2]
+//    FUNDAMENTALSLISTSDATA STRUCTURES
 
-    Task
-    Given a list lst and a number N, create a new list that contains each number of lst at most N times without reordering. For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
 
-    Example
-EnoughIsEnough.deleteNth(new int[] {20,37,20,21}, 1) // return [20,37,21]
-            EnoughIsEnough.deleteNth(new int[] {1,1,3,3,7,2,2,2,2}, 3) // return [1, 1, 3, 3, 7, 2, 2, 2]
-    FUNDAMENTALSLISTSDATA STRUCTURES
+//    The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+//
+//
+//    sleepIn(false, false) → true
+//    sleepIn(true, false) → false
+//    sleepIn(false, true) → true
+public boolean sleepIn(boolean weekday, boolean vacation) {
+    if (!weekday || vacation) {
+        return true;
+    }
+
+    return false;
 }
+    }
