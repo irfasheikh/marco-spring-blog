9bd8f252e86ddcc230168cdc9ae5d844121e0b31
Irfa Sheikh <irfasheikh@gmail.com>
2021-06-24 23:27:06 -0500

    practice prob of the day

--- src/main/java/com/codeup/springblog/controllers/Practive.java
+++ src/main/java/com/codeup/springblog/controllers/Practive.java
@@ -12,17 +12,37 @@ public class Practive {
 //    FUNDAMENTALSLISTSDATA STRUCTURES
 
 
-//    The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+    //    The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
 //
 //
 //    sleepIn(false, false) → true
 //    sleepIn(true, false) → false
 //    sleepIn(false, true) → true
-public boolean sleepIn(boolean weekday, boolean vacation) {
-    if (!weekday || vacation) {
-        return true;
-    }
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        }
 
-    return false;
-}
+        return false;
     }
+//
+//    We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+//
+//
+//    monkeyTrouble(true, true) → true
+//    monkeyTrouble(false, false) → true
+//    monkeyTrouble(true, false) → false
+
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile) {
+            return true;
+        }
+        if (!aSmile && !bSmile) {
+            return true;
+        }
+        return false;
+
+
+    }
+}
+
