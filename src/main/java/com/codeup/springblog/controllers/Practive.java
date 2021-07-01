329c075f6fa4cd53526d112f73ecbf141ee675f0
Irfa Sheikh <irfasheikh@gmail.com>
2021-06-30 23:36:30 -0500

    practice

--- src/main/java/com/codeup/springblog/controllers/Practive.java
+++ src/main/java/com/codeup/springblog/controllers/Practive.java
@@ -57,11 +57,10 @@ public class Practive {
         }
 
 
-        Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+//
 
-
-        last2("hixxhi") → 1
-        last2("xaxxaxaxx") → 1
-        last2("axxxaaxx") → 2
-
-    }
+//        public boolean makes10 ( int a, int b){
+//            return (a == 10 || b == 10 || a + b == 10);
+//        }
+//    }
+//}
