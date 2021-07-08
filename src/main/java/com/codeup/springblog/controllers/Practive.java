d738cdf78f973be9c63f414dfd309f865583c4a8
Irfa Sheikh <irfasheikh@gmail.com>
2021-07-07 22:48:27 -0500

    practice

--- src/main/java/com/codeup/springblog/controllers/Practive.java
+++ src/main/java/com/codeup/springblog/controllers/Practive.java
@@ -93,3 +93,11 @@ public class Practive {
 
                 return false;
             }
+
+            public boolean noTriples(int[] nums) {
+                // Iterate < length-2, so can use i+1 and i+2 in the loop.
+                // Return false immediately if every seeing a triple.
+                for (int i=0; i < (nums.length-2); i++) {
+                    int first = nums[i];
+                    if (nums[i+1]==first && nums[i+2]==first) return false;
+                }
