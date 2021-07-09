7b7d5b26676aeadc6fa7bca16cec6839857d9035
Irfa Sheikh <irfasheikh@gmail.com>
2021-07-08 23:56:32 -0500

    practice

--- src/main/java/com/codeup/springblog/controllers/Practive.java
+++ src/main/java/com/codeup/springblog/controllers/Practive.java
@@ -101,3 +101,18 @@ public class Practive {
                     int first = nums[i];
                     if (nums[i+1]==first && nums[i+2]==first) return false;
                 }
+
+                public String stringYak(String str) {
+                    String result = "";
+
+                    for (int i=0; i<str.length(); i++) {
+                        // Look for i starting a "yak" -- advance i in that case
+                        if (i+2<str.length() && str.charAt(i)=='y' && str.charAt(i+2)=='k') {
+                            i =  i + 2;
+                        } else { // Otherwise do the normal append
+                            result = result + str.charAt(i);
+                        }
+                    }
+
+                    return result;
+                }
