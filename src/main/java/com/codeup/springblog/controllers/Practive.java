bd127cd53110e358e9020af4a7bcad2ba12bb59a
Irfa Sheikh <irfasheikh@gmail.com>
2021-07-11 23:51:31 -0500

    practice

--- src/main/java/com/codeup/springblog/controllers/Practive.java
+++ src/main/java/com/codeup/springblog/controllers/Practive.java
@@ -116,3 +116,12 @@ public class Practive {
 
                     return result;
                 }
+
+                public boolean icyHot(int temp1, int temp2) {
+                    if ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0)) {
+                        return true;
+                    } else {
+                        return
+
+
+
