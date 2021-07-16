50ae4bf6057bae2671c2a5e803be7c340d2bc0a1
Irfa Sheikh <irfasheikh@gmail.com>
2021-07-15 23:30:35 -0500

    practice

--- src/main/java/com/codeup/springblog/controllers/Practive.java
+++ src/main/java/com/codeup/springblog/controllers/Practive.java
@@ -135,6 +135,8 @@ public class Practive {
                         public String stringX(String str) {
                             String result = "";
                             for (int i=0; i<str.length(); i++) {
+
+                            }
                                 // Only append the char if it is not the "x" case
                                 if (!(i > 0 && i < (str.length()-1) && str.substring(i, i+1).equals("x"))) {
                                     result = result + str.substring(i, i+1); // Could use str.charAt(i) here
@@ -143,4 +145,7 @@ public class Practive {
                             return result;
                         }
 
+                    paramount+ login :
+                    iffy4iffy@gmail.com
+                            carthage1010
 
