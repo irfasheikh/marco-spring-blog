6ef7cd5f0da0c4374f71634afb3d7c260f1e982e
Irfa Sheikh <irfasheikh@gmail.com>
2021-07-16 21:22:49 -0500

    practice:

--- src/main/java/com/codeup/springblog/controllers/Practive.java
+++ src/main/java/com/codeup/springblog/controllers/Practive.java
@@ -145,7 +145,16 @@ public class Practive {
                             return result;
                         }
 
-                    paramount+ login :
-                    iffy4iffy@gmail.com
-                            carthage1010
+                    public int array667(int[] nums) {
+                        int count = 0;
+                        // Note: iterate to length-1, so can use i+1 in the loop
+                        for (int i=0; i < (nums.length-1); i++) {
+                            if (nums[i] == 6) {
+                                if (nums[i+1] == 6 || nums[i+1] == 7) {
+                                    count++;
+                                }
+                            }
+                        }
+                        return count;
+                    }
 
