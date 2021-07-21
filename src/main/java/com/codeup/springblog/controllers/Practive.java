93bd87da76a3b74c508e95aee6239310bc12b4ed
Irfa Sheikh <irfasheikh@gmail.com>
2021-07-20 20:54:18 -0500

    practice

--- src/main/java/com/codeup/springblog/controllers/Practive.java
+++ src/main/java/com/codeup/springblog/controllers/Practive.java
@@ -171,3 +171,20 @@ public class Practive {
                         }
                         return result;
                     }
+
+                    public int stringMatch(String a, String b) {
+                        // Figure which string is shorter.
+                        int len = Math.min(a.length(), b.length());
+                        int count = 0;
+
+                        // Look at both substrings starting at i
+                        for (int i=0; i<len-1; i++) {
+                            String aSub = a.substring(i, i+2);
+                            String bSub = b.substring(i, i+2);
+                            if (aSub.equals(bSub)) {  // Use .equals() with strings
+                                count++;
+                            }
+                        }
+
+                        return count;
+                    }
